e1eb7a5283a8fc9e54d77a8ed184c9dc916c0c0c
Atlas <98118969+citirkaptanjack@users.noreply.github.com>
2022-06-04 13:30:20 +0300

    Create ravex.js

---
+++ ravex.js
@@ -0,0 +1,14 @@
+const pipicaneferz = "Botları kullanabilirsiniz ama kullanmanızı tavsiye etmem",
+
+const glitch = "https://glitch.com/@Eferz"
+
+const guardlar = "https://glitch.com/~spangle-joyous-buttercup"
+
+const register = "https://glitch.com/~matthe-register"
+
+const stat = "https://glitch.com/~quaint-fork-farmhouse"
+
+const welcome = "https://glitch.com/~matthe-token-gizli-welcome"
+
+const moderasyon = "https://github.com/JaylenOzi/Ozi-Bots"
+
